08229da8c9dfb5462fc768d5f91286a28f8f4242
sachinkumar9849 <englishjha@gmail.com>
2025-04-14 02:48:56 +0545

    Highlight edit and delete done

---
+++ src/app/(admin)/(others-pages)/highlight-list/[id]/page.tsx
@@ -0,0 +1,12 @@
+import HighlightEdit from '@/components/pageComponent/HighlightEdit'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <HighlightEdit/>
+    </>
+  )
+}
+
+export default page
---
+++ src/app/(admin)/(others-pages)/highlight-list/page.tsx
@@ -0,0 +1,10 @@
+import HighlightList from '@/components/pageComponent/HighlightList'
+import React from 'react'
+
+const page = () => {
+  return (
+    <HighlightList/>
+  )
+}
+
+export default page
