b7ae9583598a5b162a39791a4922d674e1a5077c
sachinkumar9849 <englishjha@gmail.com>
2025-05-26 22:43:58 +0545

    search add

--- src/app/(admin)/(others-pages)/leads/page.tsx
+++ src/app/(admin)/(others-pages)/leads/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 import React from "react";
-import ComponentCard from "@/components/common/ComponentCard";
 
 import StudentTable from "@/components/student/StudentTable";
 
 const StudentList = () => {
   return (
-    <ComponentCard title="Leads">
-      <StudentTable />
-    </ComponentCard>
+     <StudentTable />
   )
 }
 export default StudentList
